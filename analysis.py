3655d0db09bad6fc770bec5ca131028fb8892fe8
zuhause240325 <zuhause240325@gmail.com>
2026-06-07 11:05:56 +0300

    update Lviv sales to 950

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
+data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 df = pd.DataFrame(data)
 
 print("Продажі по містах:")
